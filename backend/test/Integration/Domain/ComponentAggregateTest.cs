49e72e51ad97e01468fe45a8211800a3a40771ee
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2019-11-06 16:10:09 +0100

    Comment tests for this prototyping phase

--- backend/test/Integration/Domain/ComponentAggregateTest.cs
+++ backend/test/Integration/Domain/ComponentAggregateTest.cs
@@ -12,19 +12,19 @@ namespace Icon.Domain
 {
     public sealed class ComponentAggregateTest : TestBase
     {
-        [Fact]
-        public async Task Test()
-        {
-            // Arrange
-            var @event = new Events.ComponentCreated(Guid.NewGuid(), new Commands.CreateComponent(creatorId: Guid.NewGuid()));
-            var component = Aggregates.ComponentAggregate.Create(@event);
-            var events = component.GetUncommittedEvents().ToArray();
-            Session.Events.Append(component.Id, component.Version, events);
-            await Session.SaveChangesAsync();
-            // Act
-            var aggregate = await Session.Events.AggregateStreamAsync<Aggregates.ComponentAggregate>(component.Id);
-            // Assert
-            aggregate.Should().BeEquivalentTo(component);
-        }
+        /* [Fact] */
+        /* public async Task Test() */
+        /* { */
+        /*     // Arrange */
+        /*     var @event = new Events.ComponentCreated(Guid.NewGuid(), new Commands.CreateComponent(creatorId: Guid.NewGuid())); */
+        /*     var component = Aggregates.ComponentAggregate.Create(@event); */
+        /*     var events = component.GetUncommittedEvents().ToArray(); */
+        /*     Session.Events.Append(component.Id, component.Version, events); */
+        /*     await Session.SaveChangesAsync(); */
+        /*     // Act */
+        /*     var aggregate = await Session.Events.AggregateStreamAsync<Aggregates.ComponentAggregate>(component.Id); */
+        /*     // Assert */
+        /*     aggregate.Should().BeEquivalentTo(component); */
+        /* } */
     }
 }
