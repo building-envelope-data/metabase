d8b8e731bfce192976ddb50d769b2a1e3aeb5f00
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-05-11 11:50:45 +0200

    Clean-up code and resolve open TODOs

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -8,44 +8,37 @@ using Icon.Data;
 
 namespace Icon
 {
-    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-3.0
     public class Program
     {
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-
                 try
                 {
-                    /* var context = services.GetRequiredService<MvcMovieContext>(); // TODO Why do we do this? */
                     SeedData.Initialize(services);
                 }
-                catch (Exception ex)
+                catch (Exception exception)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
+                    logger.LogError(exception, "An error occurred seeding the database.");
                 }
             }
-
             host.Run();
         }
 
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
               .ConfigureWebHostDefaults(webBuilder =>
-                  {
-                      webBuilder
-                        .UseKestrel()
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseStartup<Startup>();
-                      /* TODO? .UseIISIntegration()
-       */
-                  });
+                  webBuilder
+                  .UseKestrel() // Default web server https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-3.1
+                  .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseStartup<Startup>()
+                  );
         }
     }
 }
