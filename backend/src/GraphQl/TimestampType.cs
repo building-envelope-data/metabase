3df05839b94f48d4ce2acc33c46aacb93b4af072
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-05-12 10:28:43 +0200

    Use `Timestamp` as name for scalars of type `TimestampType`

--- backend/src/GraphQl/TimestampType.cs
+++ backend/src/GraphQl/TimestampType.cs
@@ -10,7 +10,7 @@ namespace Icon.GraphQl
     {
         public TimestampType()
           : base(
-              "TimestampType",
+              "Timestamp",
               new PreciseDateTimeType(),
               dateTimeOffset => ValueObjects.Timestamp.From(dateTimeOffset.DateTime), // TODO Shall we use DateTimeOffset to represent timestamps? They are not fully time-zone aware though as explained on https://docs.microsoft.com/en-us/dotnet/api/system.datetimeoffset?view=netcore-3.1
               timestamp => timestamp.Value
