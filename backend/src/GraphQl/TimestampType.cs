4ac567b14dd17c414affea7b4bdf2532843832a8
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-05-12 10:27:58 +0200

    Make timestamp and datetime serialization microsecond precise

--- backend/src/GraphQl/TimestampType.cs
+++ backend/src/GraphQl/TimestampType.cs
@@ -1,18 +1,17 @@
 using ValueObjects = Icon.ValueObjects;
-using DateTimeType = HotChocolate.Types.DateTimeType;
 using DateTimeOffset = System.DateTimeOffset;
 using CSharpFunctionalExtensions;
 
 namespace Icon.GraphQl
 {
-    // This type wraps `DateTimeType` which uses `DateTimeOffset`
+    // This type wraps `PreciseDateTimeType` which uses `DateTimeOffset`
     public sealed class TimestampType
         : WrappingScalarType<ValueObjects.Timestamp, DateTimeOffset>
     {
         public TimestampType()
           : base(
               "TimestampType",
-              new DateTimeType(),
+              new PreciseDateTimeType(),
               dateTimeOffset => ValueObjects.Timestamp.From(dateTimeOffset.DateTime), // TODO Shall we use DateTimeOffset to represent timestamps? They are not fully time-zone aware though as explained on https://docs.microsoft.com/en-us/dotnet/api/system.datetimeoffset?view=netcore-3.1
               timestamp => timestamp.Value
               )
