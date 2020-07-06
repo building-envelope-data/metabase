ae21d1042dadbff3f9b1f30fe445b88d6c92d42b
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-07-06 13:23:20 +0200

    Put value object infrastructure in infrastructure namespace

--- backend/src/Infrastructure/Models/IModel.cs
+++ backend/src/Infrastructure/Models/IModel.cs
@@ -1,3 +1,6 @@
+// Value objects are inspired by https://enterprisecraftsmanship.com/posts/value-object-better-implementation/
+// For the distinction between value objects and entities (what we call models) see https://enterprisecraftsmanship.com/posts/entity-vs-value-object-the-ultimate-list-of-differences/
+
 namespace Icon.Infrastructure.Models
 {
     public interface IModel
