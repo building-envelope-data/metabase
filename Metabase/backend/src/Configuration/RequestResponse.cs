b7552a02e8169b2fe3b734374f98ad4f70079a22
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-07-09 13:31:31 +0200

    Remove configuration of controllers, views, and razor pages (in particular, the configuration that changed the default location)

--- Metabase/backend/src/Configuration/RequestResponse.cs
+++ Metabase/backend/src/Configuration/RequestResponse.cs
@@ -16,44 +16,6 @@ namespace Metabase.Configuration
             // add CORS policy for non-IdentityServer endpoints
             // https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-3.0
             services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-
-            // Using `AddControllersAsServices` makes controller custructors
-            // being validated on startup with respect to the dependency
-            // injection service provider. For details see
-            // https://andrewlock.net/new-in-asp-net-core-3-service-provider-validation/#1-controller-constructor-dependencies-aren-t-checked
-            services.AddControllers()
-              .AddControllersAsServices()
-              .ConfigureApiBehaviorOptions(
-                  options =>
-                  {
-                      options.SuppressConsumesConstraintForFormFileParameters = true; // https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-3.0#multipartform-data-request-inference
-                      options.SuppressInferBindingSourcesForParameters = true; // https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-3.0#disable-inference-rules
-                                                                               // options.SuppressModelStateInvalidFilter = true;
-                                                                               // options.SuppressMapClientErrors = true;
-                                                                               // options.ClientErrorMapping[404].Link =
-                                                                               //     "https://httpstatuses.com/404";
-                  }
-                  );
-
-            // Change Default Location Of Views And Razor Pages In ASP.NET Core
-            // http://www.binaryintellect.net/articles/c50d3f14-7048-4b4f-84f4-1b28cb0f9d96.aspx
-            services.AddControllersWithViews();
-            services.Configure<RazorViewEngineOptions>(o =>
-                {
-                    o.ViewLocationFormats.Clear();
-                    o.ViewLocationFormats.Add
-            ("/Views/{1}/{0}" + RazorViewEngine.ViewExtension);
-                    o.ViewLocationFormats.Add
-            ("/Views/Shared/{0}" + RazorViewEngine.ViewExtension);
-                });
-            services.AddRazorPages(
-                options =>
-                {
-                    // https://docs.microsoft.com/en-us/aspnet/core/razor-pages/?view=aspnetcore-3.0&tabs=visual-studio-code
-                    options.RootDirectory = "/Pages";
-                    /* options.Conventions.AuthorizeFolder("/MyPages/Admin"); */
-                }
-                );
         }
 
         public static void ConfigureRouting(IApplicationBuilder app, IWebHostEnvironment environment)
