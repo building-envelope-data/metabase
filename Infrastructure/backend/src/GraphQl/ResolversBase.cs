4ad4c198531175ebd95cbbd3a6bbc476b7c03865
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-07-14 13:32:31 +0200

    Move `ResolversBase` class into `Infrastructure` project

---
+++ Infrastructure/backend/src/GraphQl/ResolversBase.cs
@@ -0,0 +1,24 @@
+namespace Infrastructure.GraphQl
+{
+    public abstract class ResolversBase
+    {
+        protected Queries.IQueryBus QueryBus { get; }
+
+        protected ResolversBase(Queries.IQueryBus queryBus)
+        {
+            QueryBus = queryBus;
+        }
+
+        protected ValueObjects.TimestampedId TimestampId(
+            ValueObjects.Id id,
+            ValueObjects.Timestamp timestamp
+            )
+        {
+            return ResultHelpers.HandleFailure(
+                ValueObjects.TimestampedId.From(
+                  id, timestamp
+                  )
+                );
+        }
+    }
+}
