c4fd6c7a2abcdc5c1f27b33e2ea8dc31d4dcdeaa
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2020-07-22 16:23:10 +0200

    Add comment to explain why `SearchComponentsPropositionInput` and `PercentagePropositionInput` are implemented the way they are

--- Infrastructure/backend/src/GraphQl/SearchComponentsPropositionInput.cs
+++ Infrastructure/backend/src/GraphQl/SearchComponentsPropositionInput.cs
@@ -1,10 +1,23 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using CSharpFunctionalExtensions;
 
 namespace Infrastructure.GraphQl
 {
+    // The current GraphQL specification from June 2018, see
+    // http://spec.graphql.org/June2018/
+    // does not allow input types to be unions. The upcoming version will
+    // probably support union input types and possible implementations can be
+    // read about on
+    // https://github.com/graphql/graphql-spec/blob/master/rfcs/InputUnion.md
+    // The discussion that lead to that document is
+    // https://github.com/graphql/graphql-spec/issues/627
+    // We therefore implement propositions, also known as boolean expressions,
+    // to search for components as seen below. This is the approach taken by
+    // Hasura, see
+    // https://hasura.io/docs/1.0/graphql/manual/api-reference/graphql-api/query.html#whereexp
+    // and
+    // https://hasura.io/docs/1.0/graphql/manual/queries/query-filters.html
     public sealed class SearchComponentsPropositionInput
     {
         public IReadOnlyList<SearchComponentsPropositionInput> And { get; }
