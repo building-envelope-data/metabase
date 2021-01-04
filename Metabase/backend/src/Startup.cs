703b9e080bd6dc1cfaac075788a9dd2bd72a768c
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-01-04 15:11:03 +0100

    Remove comments regarding `IdentityServer` and `OpenAPI` (aka `Swagger`) and `NodaTime`

--- Metabase/backend/src/Startup.cs
+++ Metabase/backend/src/Startup.cs
@@ -12,28 +12,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-// TODO ? Certificate authentication: https://docs.microsoft.com/en-us/aspnet/core/security/authentication/certauth?view=aspnetcore-3.0
-// TODO Use NodaTime (see commented code below; confer https://www.npgsql.org/efcore/mapping/nodatime.html).
-//      There are problems with the generated migrations of the identity server.
-//      Maybe https://github.com/etiennemtl/identity-npsql-nodatime helps.
-//      The error was `System.InvalidCastException: Can't write CLR type System.DateTime with handler type TimestampHandler`
-//      See also https://github.com/npgsql/Npgsql.EntityFrameworkCore.PostgreSQL/issues/568
-//      https://github.com/npgsql/Npgsql.EntityFrameworkCore.PostgreSQL/issues/648
-// TODO API versioning: https://github.com/RicoSuter/NSwag/issues/2118
-
-// OpenIddict is another implementation of OpenId Connect
-
-// IdentityServer4 Demo: https://demo.identityserver.io/
-//                       https://github.com/IdentityServer/IdentityServer4.Demo/blob/master/src/IdentityServer4Demo/Config.cs
-
-// TODO IdentityServer4 Admin UI. Available options are:
-//      https://github.com/skoruba/IdentityServer4.Admin
-//      https://github.com/brunohbrito/JPProject.IdentityServer4.AdminUI
-//      https://github.com/zarxor/IdentityServer4.OpenAdmin
+// TODO Certificate authentication: https://docs.microsoft.com/en-us/aspnet/core/security/authentication/certauth
 
 // TODO For client libraries use https://identitymodel.readthedocs.io/en/latest/
-
-// TODO Swagger UI for IdentityServer4 endpoints, follow `https://github.com/IdentityServer/IdentityServer4/issues/2286`
 
 namespace Metabase
 {
