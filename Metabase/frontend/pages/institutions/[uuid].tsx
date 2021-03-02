1ad598db357956a0a9f7f526888d3299e1833fe6
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-03-02 19:22:37 +0100

    List and add manufactured components

--- Metabase/frontend/pages/institutions/[uuid].tsx
+++ Metabase/frontend/pages/institutions/[uuid].tsx
@@ -1,50 +1,87 @@
-import Layout from '../../components/Layout'
-import { Typography, message, Skeleton } from "antd"
-import { useInstitutionQuery } from "../../queries/institutions.graphql"
-import { useRouter } from 'next/router'
+import Layout from "../../components/Layout";
+import { Divider, List, Card, Typography, message, Skeleton } from "antd";
+import { EditOutlined, MoreOutlined } from "@ant-design/icons";
+import { useInstitutionQuery } from "../../queries/institutions.graphql";
+import { useRouter } from "next/router";
+import { useCurrentUserQuery } from "../../queries/currentUser.graphql";
+import CreateComponent from "../../components/components/CreateComponent";
 
-function Institution() {
-    const router = useRouter()
-    const { uuid } = router.query
-    const { loading, error, data } = useInstitutionQuery({
-        variables: {
-            uuid: uuid
+function Show() {
+  const router = useRouter();
+
+  const currentUserQuery = useCurrentUserQuery();
+  const currentUser = currentUserQuery.data?.currentUser;
+
+  const { uuid } = router.query;
+  const { loading, error, data } = useInstitutionQuery({
+    variables: {
+      uuid: uuid,
+    },
+  });
+  const institution = data?.institution;
+
+  if (error) {
+    message.error(error);
+  }
+
+  if (loading || !institution) {
+    return <Layout></Layout>;
+  }
+
+  return (
+    <Layout>
+      {loading || !institution ? (
+        <Skeleton active avatar title />
+      ) : (
+        <>
+          <Typography.Title>
+            {institution.websiteLocator ? (
+              <Typography.Link href={institution.websiteLocator}>
+                {`${institution.name} (${institution.abbreviation})`}
+              </Typography.Link>
+            ) : (
+              `${institution.name} (${institution.abbreviation})`
+            )}
+          </Typography.Title>
+          <Typography.Text>{institution.description}</Typography.Text>
+        </>
+      )}
+      <Divider />
+      <Typography.Title level={2}>Manufactured Components</Typography.Title>
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 2,
+          md: 4,
+          lg: 4,
+          xl: 6,
+          xxl: 3,
+        }}
+        dataSource={
+          institution?.manufacturedComponents?.edges?.map((x) => x.node) || []
         }
-    })
-    const institution = data?.institution
-
-    if (error) {
-        message.error(error)
-    }
-
-    if (loading || !institution) {
-        return (
-            <Layout>
-                <Skeleton
-                    active
-                    avatar
-                    title
-                    loading={loading}
-                />
-            </Layout>
-        )
-    }
-
-    return (
-        <Layout>
-            <Typography.Title>
-                {institution.websiteLocator
-                    ?
-                    <Typography.Link href={institution.websiteLocator}>
-                        {`${institution.name} (${institution.abbreviation})`}
-                    </Typography.Link>
-                    : `${institution.name} (${institution.abbreviation})`}
-            </Typography.Title>
-            <Typography.Text>
-                {institution.description}
-            </Typography.Text>
-        </Layout >
-    )
+        renderItem={(item) => (
+          <Card
+            actions={[<EditOutlined key="edit" />, <MoreOutlined key="more" />]}
+          >
+            <Skeleton loading={loading} active>
+              <Card.Meta title={item.name} description={item.description} />
+            </Skeleton>
+          </Card>
+        )}
+      />
+      {currentUser ? (
+        <CreateComponent manufacturerId={institution.uuid} />
+      ) : (
+        <></>
+      )}
+      <Divider />
+      <Typography.Title level={2}>Operated Databases</Typography.Title>
+      <Divider />
+      <Typography.Title level={2}>Representatives</Typography.Title>
+    </Layout>
+  );
 }
 
-export default Institution
+export default Show;
