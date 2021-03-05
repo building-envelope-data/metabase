bff2aa59c80038a0f9a07dcd7eaf3a168cbc1ece
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-03-05 12:56:40 +0100

    Add role to user represented institution edge

--- Metabase/backend/src/GraphQl/Users/UserRepresentedInstitutionEdge.cs
+++ Metabase/backend/src/GraphQl/Users/UserRepresentedInstitutionEdge.cs
@@ -5,11 +5,14 @@ namespace Metabase.GraphQl.Users
     public sealed class UserRepresentedInstitutionEdge
         : Edge<Data.Institution, InstitutionByIdDataLoader>
     {
+        public Enumerations.InstitutionRepresentativeRole Role { get; }
+
         public UserRepresentedInstitutionEdge(
             Data.InstitutionRepresentative association
         )
             : base(association.InstitutionId)
         {
+            Role = association.Role;
         }
     }
 }
