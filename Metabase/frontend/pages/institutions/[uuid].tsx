eac85fa305ac2cc3d58634bf0677bf40ffe9c213
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-03-09 18:20:39 +0100

    Move contant declaration

--- Metabase/frontend/pages/institutions/[uuid].tsx
+++ Metabase/frontend/pages/institutions/[uuid].tsx
@@ -4,12 +4,13 @@ import Layout from "../../components/Layout";
 
 function Show() {
   const router = useRouter();
-  const { uuid } = router.query;
 
   if (!router.isReady) {
     // Otherwise `uuid`, aka, `router.query`, is null on first render, see https://github.com/vercel/next.js/discussions/11484
     return null;
   }
+
+  const { uuid } = router.query;
 
   return (
     <Layout>
