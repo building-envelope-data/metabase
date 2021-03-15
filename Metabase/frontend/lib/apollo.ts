ac3d9c13efced56c7857520960685eb5b8295133
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-03-15 19:13:46 +0100

    Configure the Apollo Client to pass the Authorization header with the Access Token as Bearer in each GraphQL request

--- Metabase/frontend/lib/apollo.ts
+++ Metabase/frontend/lib/apollo.ts
@@ -1,68 +1,78 @@
-import { IncomingMessage, ServerResponse } from 'http'
-import { useMemo } from 'react'
+import { IncomingMessage, ServerResponse } from "http";
+import { useMemo } from "react";
 import {
   ApolloClient,
   createHttpLink,
   InMemoryCache,
-  NormalizedCacheObject
-} from '@apollo/client'
-import merge from 'deepmerge'
+  NormalizedCacheObject,
+} from "@apollo/client";
+import merge from "deepmerge";
 
-let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
 export type ResolverContext = {
-  req?: IncomingMessage
-  res?: ServerResponse
-}
+  req?: IncomingMessage;
+  res?: ServerResponse;
+};
 
-function createIsomorphLink(context: ResolverContext = {}) {
+function createIsomorphLink(
+  context: ResolverContext = {},
+  accessToken?: string
+) {
   return createHttpLink({
-    uri: typeof window === 'undefined'
-      ? 'http://backend:8080/graphql/'
-      : `${process.env.NEXT_PUBLIC_METABASE_URL}/graphql/`,
+    uri:
+      typeof window === "undefined"
+        ? "http://backend:8080/graphql/"
+        : `${process.env.NEXT_PUBLIC_METABASE_URL}/graphql/`,
     useGETForQueries: true,
-    credentials: 'same-origin',
+    credentials: "same-origin",
     headers: {
+      authorization: accessToken ? `Bearer ${accessToken}` : "",
       cookie: context.req ? context.req.headers.cookie : null,
     },
-  })
+  });
 }
 
-function createApolloClient(context?: ResolverContext) {
+function createApolloClient(context?: ResolverContext, accessToken?: string) {
   return new ApolloClient({
-    ssrMode: typeof window === 'undefined',
-    link: createIsomorphLink(context),
+    ssrMode: typeof window === "undefined",
+    link: createIsomorphLink(context, accessToken),
     cache: new InMemoryCache(),
-  })
+  });
 }
 
 export function initializeApollo(
   initialState: any = null,
+  accessToken?: string,
   // Pages with Next.js data fetching methods, like `getStaticProps`, can send
   // a custom context which will be used by `SchemaLink` to server render pages
   context?: ResolverContext
 ) {
-  const _apolloClient = apolloClient ?? createApolloClient(context)
+  const _apolloClient =
+    apolloClient ?? createApolloClient(context, accessToken);
 
   // If your page has Next.js data fetching methods that use Apollo Client, the initial state
   // get hydrated here
   if (initialState) {
     // Get existing cache, loaded during client side data fetching
-    const existingCache = _apolloClient.extract()
+    const existingCache = _apolloClient.extract();
     // Merge the existing cache into data passed from getStaticProps/getServerSideProps
-    const data = merge(initialState, existingCache)
+    const data = merge(initialState, existingCache);
     // Restore the cache with the merged data
-    _apolloClient.cache.restore(data)
+    _apolloClient.cache.restore(data);
   }
   // For SSG and SSR always create a new Apollo Client
-  if (typeof window === 'undefined') return _apolloClient
+  if (typeof window === "undefined") return _apolloClient;
   // Create the Apollo Client once in the client
-  if (!apolloClient) apolloClient = _apolloClient
+  if (!apolloClient) apolloClient = _apolloClient;
 
-  return _apolloClient
+  return _apolloClient;
 }
 
-export function useApollo(initialState: any) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState])
-  return store
+export function useApollo(initialState: any, accessToken?: string) {
+  const store = useMemo(() => initializeApollo(initialState, accessToken), [
+    initialState,
+    accessToken,
+  ]);
+  return store;
 }
--- Metabase/frontend/pages/_app.tsx
+++ Metabase/frontend/pages/_app.tsx
@@ -2,19 +2,55 @@ import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apollo";
+import { useSession } from "next-auth/client";
 
 import "antd/dist/antd.css";
 import "../styles/vars.css";
 import "../styles/global.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
+// We first use `Provider` from `next-auth/client` to allow the usage of
+// `useSession`. We then use `useSession` to fetch the session, in particular,
+// its access token. The access token is then used in `useApollo` as `Bearer` in
+// the `Authorization` header for all GraphQL requests subsequently done via
+// Apollo.
+
+export default function App(appProps: AppProps) {
+  return (
+    <Provider session={appProps.pageProps.session}>
+      <UseSession {...appProps} />
+    </Provider>
+  );
+}
+
+function UseSession(appProps: AppProps) {
+  const [session, loading] = useSession();
+
+  // `useSession` above makes a request to `/api/auth/session` and waits for it
+  // to finish making it the bottleneck. Instead of doing that to add the access
+  // token as `Authorization` Bearer request header to the Apollo client, we
+  // could add an endpoint `/api/graphql` that redirects to the backend GraphQL
+  // endpoint as remarked on https://github.com/nextauthjs/next-auth/issues/643
+  // (see also https://next-auth.js.org/tutorials/securing-pages-and-api-routes)
+  if (loading) {
+    return null;
+  }
+
+  return <UseApollo session={session} {...appProps} />;
+}
+
+function UseApollo({
+  session,
+  Component,
+  pageProps,
+}: { session: { accessToken?: string } | null | undefined } & AppProps) {
+  const apolloClient = useApollo(
+    pageProps.initialApolloState,
+    session?.accessToken
+  );
 
   return (
-    <Provider session={pageProps.session}>
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </Provider>
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
