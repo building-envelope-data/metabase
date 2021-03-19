961c6df07ff8b81dc07ba4bd2d3359134ecc6ec5
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-03-19 16:43:59 +0100

    Allow `null` in `ErrorViewModel` properties

--- backend/src/ViewModels/Shared/ErrorViewModel.cs
+++ backend/src/ViewModels/Shared/ErrorViewModel.cs
@@ -5,9 +5,9 @@ namespace Metabase.ViewModels.Shared
     public class ErrorViewModel
     {
         [Display(Name = "Error")]
-        public string Error { get; set; }
+        public string? Error { get; set; }
 
         [Display(Name = "Description")]
-        public string ErrorDescription { get; set; }
+        public string? ErrorDescription { get; set; }
     }
 }
