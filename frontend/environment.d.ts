7b3600cfca9cbe04b5d786a9416d0494da4932b0
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-04-08 16:48:25 +0200

    Remove obsolete environment variables

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -2,12 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_METABASE_URL: string;
-      NEXTAUTH_URL: string;
       NODE_ENV: "test" | "development" | "production";
-      AUTH_CLIENT_ID: string;
-      AUTH_CLIENT_SECRET: string;
-      AUTH_SECRET: string;
-      AUTH_JWT_SECRET: string;
     }
   }
 }
