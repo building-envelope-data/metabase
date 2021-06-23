cfaf718b4c06916f64689b0e9f3977ba17b1811f
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-06-23 17:28:43 +0200

    Add field `allOpticalData` to database whose value is fetched by forwarding the query to the database's GraphQL endpoint

--- backend/src/GraphQl/Databases/DatabaseResolvers.cs
+++ backend/src/GraphQl/Databases/DatabaseResolvers.cs
@@ -1,23 +1,134 @@
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Metabase.GraphQl.Institutions;
+using GraphQL.Client.Http;
+using Microsoft.Extensions.Logging;
+using GraphQL.Client.Serializer.SystemTextJson;
+using System;
+using HotChocolate;
 
 namespace Metabase.GraphQl.Databases
 {
     public class DatabaseResolvers
     {
-        public async Task<Data.Institution> GetOperatorAsync(
+        private readonly ILogger<DatabaseResolvers> _logger;
+
+        public DatabaseResolvers(
+            ILogger<DatabaseResolvers> logger
+        )
+        {
+            _logger = logger;
+        }
+
+        public string Hi(
             Data.Database database,
-            InstitutionByIdDataLoader institutionById,
+            DataX.OpticalDataPropositionInput where,
+            DateTime? timestamp,
+            string? locale,
+            int? first,
+            string? after,
+            int? last,
+            string? before,
+            CancellationToken cancellationToken
+        ) {
+            return "";
+        }
+
+        public Task<DataX.DataConnection?> GetAllOpticalDataAsync(
+            Data.Database database,
+            DataX.OpticalDataPropositionInput where,
+            DateTime? timestamp,
+            string? locale,
+            /*TODO add `u`*/int? first,
+            string? after,
+            /*TODO add `u`*/int? last,
+            string? before,
             CancellationToken cancellationToken
             )
         {
-            return (
-                await institutionById.LoadAsync(
-                database.OperatorId,
-                 cancellationToken
-                 ).ConfigureAwait(false)
-                 )!;
+            return QueryDatabase<DataX.DataConnection>(
+                database,
+                new GraphQL.GraphQLRequest(
+                    query: File.ReadAllText("GraphQl/Databases/AllOpticalData.graphql"),
+                    variables: new
+                    {
+                        where,
+                        timestamp,
+                        locale,
+                        first,
+                        after,
+                        last,
+                        before
+                    },
+                    operationName: "AllOpticalData"
+                ),
+                cancellationToken
+            );
+        }
+
+        private async
+          Task<TGraphQlResponse?>
+          QueryDatabase<TGraphQlResponse>(
+            Data.Database database,
+            GraphQL.GraphQLRequest request,
+            CancellationToken cancellationToken
+            )
+          where TGraphQlResponse : class
+        {
+            // https://github.com/graphql-dotnet/graphql-client/blob/47b4abfbfda507a91b5c62a18a9789bd3a8079c7/src/GraphQL.Client/GraphQLHttpResponse.cs
+            try
+            {
+                var response =
+                  (
+                   await CreateGraphQlClient(database)
+                   .SendQueryAsync<TGraphQlResponse>(
+                     request,
+                     cancellationToken
+                     )
+                   .ConfigureAwait(false)
+                   )
+                  .AsGraphQLHttpResponse();
+                if (
+                    response.StatusCode != System.Net.HttpStatusCode.OK ||
+                    response.Errors?.Length >= 1 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/nullable-value-types#lifted-operators
+                    )
+                {
+                    // TODO Report errors to client? With error code `ErrorCodes.GraphQlRequestFailed`?
+                    _logger.LogWarning($"Accessing the database {database.Locator} failed with status code {response.StatusCode} and errors {(response.Errors is null ? "" : string.Join(", ", response.Errors.Select(GraphQlErrorToString)))}");
+                    return null;
+                }
+                return response.Data;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+
+        private static string GraphQlErrorToString(
+            GraphQL.GraphQLError error
+            )
+        {
+            return $"GraphQlError(message: {error.Message}, locations: [{(error.Locations is null ? "" : string.Join(", ", error.Locations.Select(GraphQlLocationToString)))}], path: [{(error.Path is null ? "" : string.Join(", ", error.Path))}], extensions: {(error.Extensions is null ? "[]" : string.Join(", ", error.Extensions))})";
+        }
+
+        private static string GraphQlLocationToString(
+            GraphQL.GraphQLLocation location
+            )
+        {
+            return $"{location.Line}:{location.Column}";
+        }
+
+        private static GraphQLHttpClient CreateGraphQlClient(
+            Data.Database database
+            )
+        {
+            return new GraphQLHttpClient(
+                endPoint: database.Locator.AbsoluteUri,
+                serializer: new SystemTextJsonSerializer()
+                );
         }
     }
 }
--- backend/src/GraphQl/Databases/DatabaseType.cs
+++ backend/src/GraphQl/Databases/DatabaseType.cs
@@ -20,6 +20,18 @@ namespace Metabase.GraphQl.Databases
                     );
             descriptor
                 .Field(t => t.OperatorId).Ignore();
+            descriptor
+                .Field("allOpticalData")
+                .Argument("where", _ => _.Type<NonNullType<InputObjectType<DataX.OpticalDataPropositionInput>>>())
+                .Argument("timestamp", _ => _.Type<DateTimeType>())
+                .Argument("locale", _ => _.Type<StringType>())
+                .Argument("first", _ => _.Type</*TODO Unsigned*/IntType>())
+                .Argument("after", _ => _.Type<StringType>())
+                .Argument("last", _ => _.Type</*TODO Unsigned*/IntType>())
+                .Argument("before", _ => _.Type<StringType>())
+                .ResolveWith<DatabaseResolvers>(_ =>
+                    _.GetAllOpticalDataAsync(default!, default!, default!, default!, default!, default!, default!, default!, default!)
+                );
         }
     }
 }
