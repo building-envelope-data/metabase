20ad18dcc4da6ce4d5275a5f526198826a86e992
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-07-08 17:42:13 +0200

    Prepare database migration from `InitialCreate` to `RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations`

---
+++ backend/src/Migrations/20210708152541_RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Metabase.Migrations
+{
+    public partial class RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DataFormats_institution_ManagerId",
+                schema: "metabase",
+                table: "DataFormats");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DataFormats",
+                schema: "metabase",
+                table: "DataFormats");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "metabase",
+                table: "user_method_developer");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                schema: "metabase",
+                table: "user_method_developer");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "metabase",
+                table: "institution_method_developer");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                schema: "metabase",
+                table: "institution_method_developer");
+
+            migrationBuilder.RenameTable(
+                name: "DataFormats",
+                schema: "metabase",
+                newName: "data_format",
+                newSchema: "metabase");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DataFormats_ManagerId",
+                schema: "metabase",
+                table: "data_format",
+                newName: "IX_data_format_ManagerId");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:public.component_category", "material,layer,unit")
+                .Annotation("Npgsql:Enum:public.institution_representative_role", "owner,maintainer,assistant")
+                .Annotation("Npgsql:Enum:public.institution_state", "unknown,operative,inoperative")
+                .Annotation("Npgsql:Enum:public.method_category", "measurement,calculation")
+                .Annotation("Npgsql:Enum:public.standardizer", "aerc,agi,ashrae,breeam,bs,bsi,cen,cie,dgnb,din,dvwg,iec,ies,ift,iso,jis,leed,nfrc,riba,ul,unece,vdi,vff,well")
+                .Annotation("Npgsql:PostgresExtension:pgcrypto", ",,")
+                .OldAnnotation("Npgsql:Enum:component_category", "material,layer,unit")
+                .OldAnnotation("Npgsql:Enum:institution_representative_role", "owner,maintainer,assistant")
+                .OldAnnotation("Npgsql:Enum:institution_state", "unknown,operative,inoperative")
+                .OldAnnotation("Npgsql:Enum:method_category", "measurement,calculation")
+                .OldAnnotation("Npgsql:Enum:standardizer", "aerc,agi,ashrae,breeam,bs,bsi,cen,cie,dgnb,din,dvwg,iec,ies,ift,iso,jis,leed,nfrc,riba,ul,unece,vdi,vff,well")
+                .OldAnnotation("Npgsql:PostgresExtension:pgcrypto", ",,");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "xmin",
+                schema: "metabase",
+                table: "data_format",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                schema: "metabase",
+                table: "data_format",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_data_format",
+                schema: "metabase",
+                table: "data_format",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_data_format_institution_ManagerId",
+                schema: "metabase",
+                table: "data_format",
+                column: "ManagerId",
+                principalSchema: "metabase",
+                principalTable: "institution",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_data_format_institution_ManagerId",
+                schema: "metabase",
+                table: "data_format");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_data_format",
+                schema: "metabase",
+                table: "data_format");
+
+            migrationBuilder.RenameTable(
+                name: "data_format",
+                schema: "metabase",
+                newName: "DataFormats",
+                newSchema: "metabase");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_data_format_ManagerId",
+                schema: "metabase",
+                table: "DataFormats",
+                newName: "IX_DataFormats_ManagerId");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:component_category", "material,layer,unit")
+                .Annotation("Npgsql:Enum:institution_representative_role", "owner,maintainer,assistant")
+                .Annotation("Npgsql:Enum:institution_state", "unknown,operative,inoperative")
+                .Annotation("Npgsql:Enum:method_category", "measurement,calculation")
+                .Annotation("Npgsql:Enum:standardizer", "aerc,agi,ashrae,breeam,bs,bsi,cen,cie,dgnb,din,dvwg,iec,ies,ift,iso,jis,leed,nfrc,riba,ul,unece,vdi,vff,well")
+                .Annotation("Npgsql:PostgresExtension:pgcrypto", ",,")
+                .OldAnnotation("Npgsql:Enum:public.component_category", "material,layer,unit")
+                .OldAnnotation("Npgsql:Enum:public.institution_representative_role", "owner,maintainer,assistant")
+                .OldAnnotation("Npgsql:Enum:public.institution_state", "unknown,operative,inoperative")
+                .OldAnnotation("Npgsql:Enum:public.method_category", "measurement,calculation")
+                .OldAnnotation("Npgsql:Enum:public.standardizer", "aerc,agi,ashrae,breeam,bs,bsi,cen,cie,dgnb,din,dvwg,iec,ies,ift,iso,jis,leed,nfrc,riba,ul,unece,vdi,vff,well")
+                .OldAnnotation("Npgsql:PostgresExtension:pgcrypto", ",,");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                schema: "metabase",
+                table: "user_method_developer",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<long>(
+                name: "xmin",
+                schema: "metabase",
+                table: "user_method_developer",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                schema: "metabase",
+                table: "institution_method_developer",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<long>(
+                name: "xmin",
+                schema: "metabase",
+                table: "institution_method_developer",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "xmin",
+                schema: "metabase",
+                table: "DataFormats",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "xid",
+                oldRowVersion: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                schema: "metabase",
+                table: "DataFormats",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "gen_random_uuid()");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DataFormats",
+                schema: "metabase",
+                table: "DataFormats",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DataFormats_institution_ManagerId",
+                schema: "metabase",
+                table: "DataFormats",
+                column: "ManagerId",
+                principalSchema: "metabase",
+                principalTable: "institution",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ backend/src/Migrations/migrate_from_20210414155531_InitialCreate_to_20210708152541_RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations.sql
@@ -0,0 +1,37 @@
+﻿START TRANSACTION;
+
+ALTER TABLE metabase."DataFormats" DROP CONSTRAINT "FK_DataFormats_institution_ManagerId";
+
+ALTER TABLE metabase."DataFormats" DROP CONSTRAINT "PK_DataFormats";
+
+ALTER TABLE metabase.user_method_developer DROP COLUMN "Id";
+
+ALTER TABLE metabase.institution_method_developer DROP COLUMN "Id";
+
+ALTER TABLE metabase."DataFormats" RENAME TO data_format;
+
+ALTER INDEX metabase."IX_DataFormats_ManagerId" RENAME TO "IX_data_format_ManagerId";
+
+CREATE TYPE public.component_category AS ENUM ('material', 'layer', 'unit');
+CREATE TYPE public.institution_representative_role AS ENUM ('owner', 'maintainer', 'assistant');
+CREATE TYPE public.institution_state AS ENUM ('unknown', 'operative', 'inoperative');
+CREATE TYPE public.method_category AS ENUM ('measurement', 'calculation');
+CREATE TYPE public.standardizer AS ENUM ('aerc', 'agi', 'ashrae', 'breeam', 'bs', 'bsi', 'cen', 'cie', 'dgnb', 'din', 'dvwg', 'iec', 'ies', 'ift', 'iso', 'jis', 'leed', 'nfrc', 'riba', 'ul', 'unece', 'vdi', 'vff', 'well');
+DROP TYPE metabase.component_category;
+DROP TYPE metabase.institution_representative_role;
+DROP TYPE metabase.institution_state;
+DROP TYPE metabase.method_category;
+DROP TYPE metabase.standardizer;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+ALTER TABLE metabase.data_format ALTER COLUMN "Id" SET DEFAULT (gen_random_uuid());
+
+ALTER TABLE metabase.data_format ADD CONSTRAINT "PK_data_format" PRIMARY KEY ("Id");
+
+ALTER TABLE metabase.data_format ADD CONSTRAINT "FK_data_format_institution_ManagerId" FOREIGN KEY ("ManagerId") REFERENCES metabase.institution ("Id") ON DELETE CASCADE;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20210708152541_RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations', '5.0.7');
+
+COMMIT;
+
