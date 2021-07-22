af3a37f4ee714bd0f02826b1c0904ff9d7690286
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-07-22 15:59:04 +0200

    Add migration and rollback SQL files from RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations to AddManagerRelationToInstitution

---
+++ backend/src/Migrations/migrate_from_20210708152541_RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations_to_20210715150119_AddManagerRelationToInstitution.sql
@@ -0,0 +1,37 @@
+﻿START TRANSACTION;
+
+ALTER TABLE metabase.user_method_developer ADD "Pending" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE metabase.method ADD "ManagerId" uuid NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
+
+ALTER TABLE metabase.institution_method_developer ADD "Pending" boolean NOT NULL DEFAULT TRUE;
+
+CREATE INDEX "IX_method_ManagerId" ON metabase.method ("ManagerId");
+
+ALTER TABLE metabase.method ADD CONSTRAINT "FK_method_institution_ManagerId" FOREIGN KEY ("ManagerId") REFERENCES metabase.institution ("Id") ON DELETE CASCADE;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20210712143023_AddMethodManagerOneToManyRelation', '5.0.8');
+
+COMMIT;
+
+START TRANSACTION;
+
+ALTER TYPE institution_representative_role RENAME TO institution_representative_role_old;
+CREATE TYPE institution_representative_role AS ENUM ('owner', 'assistant');
+ALTER TABLE metabase.institution_representative ALTER COLUMN "Role" TYPE institution_representative_role USING "Role"::text::institution_representative_role;
+DROP TYPE institution_representative_role_old;
+
+ALTER TABLE metabase.institution_representative ADD "Pending" boolean NOT NULL DEFAULT TRUE;
+
+ALTER TABLE metabase.institution ADD "ManagerId" uuid NULL;
+
+CREATE INDEX "IX_institution_ManagerId" ON metabase.institution ("ManagerId");
+
+ALTER TABLE metabase.institution ADD CONSTRAINT "FK_institution_institution_ManagerId" FOREIGN KEY ("ManagerId") REFERENCES metabase.institution ("Id") ON DELETE RESTRICT;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20210715150119_AddManagerRelationToInstitution', '5.0.8');
+
+COMMIT;
+
---
+++ backend/src/Migrations/rollback_from_20210708152541_RenameDataFormatsTableAndRemoveIdAndXminFromMethodDeveloperRelations_to_20210715150119_AddManagerRelationToInstitution.sql
@@ -0,0 +1,37 @@
+﻿START TRANSACTION;
+
+ALTER TABLE metabase.institution DROP CONSTRAINT "FK_institution_institution_ManagerId";
+
+DROP INDEX metabase."IX_institution_ManagerId";
+
+ALTER TABLE metabase.institution_representative DROP COLUMN "Pending";
+
+ALTER TABLE metabase.institution DROP COLUMN "ManagerId";
+
+CREATE SCHEMA IF NOT EXISTS metabase;
+
+CREATE TYPE metabase.institution_representative_role AS ENUM ('owner', 'maintainer', 'assistant');
+DROP TYPE public.institution_representative_role;
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20210715150119_AddManagerRelationToInstitution';
+
+COMMIT;
+
+START TRANSACTION;
+
+ALTER TABLE metabase.method DROP CONSTRAINT "FK_method_institution_ManagerId";
+
+DROP INDEX metabase."IX_method_ManagerId";
+
+ALTER TABLE metabase.user_method_developer DROP COLUMN "Pending";
+
+ALTER TABLE metabase.method DROP COLUMN "ManagerId";
+
+ALTER TABLE metabase.institution_method_developer DROP COLUMN "Pending";
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20210712143023_AddMethodManagerOneToManyRelation';
+
+COMMIT;
+
