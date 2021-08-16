63a14e4d74fae10c4e7e004f4a0f2021d5f32df3
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-08-16 17:10:47 +0200

    Add migration and rollback SQL files from 20210722150010_AddPendingColumnToComponentManufacturer to 20210805142241_UsePendingAndConfirmedAsInstitutionStates

---
+++ backend/src/Migrations/rollback_from_20210722150010_AddPendingColumnToComponentManufacturer_to_20210805142241_UsePendingAndConfirmedAsInstitutionStates.sql
@@ -0,0 +1,14 @@
+﻿START TRANSACTION;
+
+CREATE SCHEMA IF NOT EXISTS metabase;
+
+ALTER TYPE institution_state RENAME TO institution_state_old;
+CREATE TYPE institution_state AS ENUM ('unknown', 'operative', 'inoperative');
+ALTER TABLE metabase.institution ALTER COLUMN "State" TYPE institution_state USING 'operative';
+DROP TYPE institution_state_old;
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20210805142241_UsePendingAndConfirmedAsInstitutionStates';
+
+COMMIT;
+
