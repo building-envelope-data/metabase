db65495dc78e8d93bb4d68a6c957b25c82cb3bd5
jf-ise <joerg.fennert@ise.fraunhofer.de>
2021-08-23 11:45:04 +0200

    Set preconditions for nunit-tests.

---
+++ backend/ntest/UnitNTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace Metabase.NTests
+{
+    public class UnitNTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
