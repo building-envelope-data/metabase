110b2e0fec32642476ea39bcab6f60182a87dc8a
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-11-02 14:45:00 +0100

    Add TODO regarding highlighted values

--- frontend/lib/table.tsx
+++ frontend/lib/table.tsx
@@ -577,6 +577,7 @@ export function getAppliedMethodColumnProps<
         title: "UUID",
         value: x.appliedMethod.methodId,
         render: (_record, _highlightedValue, value) => (
+          // TODO Why does this not work with `_highlightedValue`? An error is raised saying "Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?": https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-function-component or https://reactjs.org/docs/forwarding-refs.html or https://deepscan.io/docs/rules/react-func-component-invalid-ref-prop or https://www.carlrippon.com/react-forwardref-typescript/
           <Link href={paths.method(x.appliedMethod.methodId)}>{value}</Link>
         ),
       },
@@ -633,6 +634,7 @@ export function getResourceTreeColumnProps<
         title: "Format UUID",
         value: x.resourceTree.root.value.formatId,
         render: (_record, _hightlightedValue, value) => (
+          // TODO Why does this not work with `_highlightedValue`? An error is raised saying "Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?": https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-function-component or https://reactjs.org/docs/forwarding-refs.html or https://deepscan.io/docs/rules/react-func-component-invalid-ref-prop or https://www.carlrippon.com/react-forwardref-typescript/
           <Link href={paths.dataFormat(x.resourceTree.root.value.formatId)}>
             {value}
           </Link>
