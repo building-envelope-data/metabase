c0cdcd5a25b723584e80da12e785acaef2e5c7e7
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-11-04 15:43:51 +0100

    Run tests without SDK

---
+++ backend/test/Program.cs
@@ -0,0 +1,14 @@
+using NUnitLite;
+
+namespace Metabase.Tests
+{
+    public class Program
+    {
+      public static int Main(
+          string[] commandLineArguments
+      )
+          {
+              return new AutoRun().Execute(commandLineArguments);
+          }
+    }
+}
