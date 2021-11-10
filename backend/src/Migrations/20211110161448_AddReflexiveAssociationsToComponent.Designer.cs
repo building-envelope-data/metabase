ab6a84190a0102ec24e4fcdc9748458f9b305efe
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-11-10 17:16:38 +0100

    Add database migration for reflective component associations

--- backend/src/Migrations/20211110161448_AddReflexiveAssociationsToComponent.Designer.cs
+++ backend/src/Migrations/20211110161448_AddReflexiveAssociationsToComponent.Designer.cs
@@ -4,6 +4,7 @@ using Metabase.Data;
 using Metabase.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -11,9 +12,10 @@ using NpgsqlTypes;
 namespace Metabase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211110161448_AddReflexiveAssociationsToComponent")]
+    partial class AddReflexiveAssociationsToComponent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
