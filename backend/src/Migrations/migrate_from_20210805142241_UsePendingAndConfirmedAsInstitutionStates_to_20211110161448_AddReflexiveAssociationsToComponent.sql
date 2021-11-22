0bb81fb0078856cc726f12e2d8dd397158b3922c
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-11-22 15:34:56 +0100

    Generate SQL migrations that add reflexive associations to component by running `make prepare-release`

---
+++ backend/src/Migrations/migrate_from_20210805142241_UsePendingAndConfirmedAsInstitutionStates_to_20211110161448_AddReflexiveAssociationsToComponent.sql
@@ -0,0 +1,29 @@
+﻿START TRANSACTION;
+
+ALTER TABLE metabase.component_concretization_and_generalization DROP COLUMN "Id";
+
+CREATE TABLE metabase.component_assembly (
+    "AssembledComponentId" uuid NOT NULL,
+    "PartComponentId" uuid NOT NULL,
+    CONSTRAINT "PK_component_assembly" PRIMARY KEY ("AssembledComponentId", "PartComponentId"),
+    CONSTRAINT "FK_component_assembly_component_AssembledComponentId" FOREIGN KEY ("AssembledComponentId") REFERENCES metabase.component ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_component_assembly_component_PartComponentId" FOREIGN KEY ("PartComponentId") REFERENCES metabase.component ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE metabase.component_variant (
+    "OfComponentId" uuid NOT NULL,
+    "ToComponentId" uuid NOT NULL,
+    CONSTRAINT "PK_component_variant" PRIMARY KEY ("OfComponentId", "ToComponentId"),
+    CONSTRAINT "FK_component_variant_component_OfComponentId" FOREIGN KEY ("OfComponentId") REFERENCES metabase.component ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_component_variant_component_ToComponentId" FOREIGN KEY ("ToComponentId") REFERENCES metabase.component ("Id") ON DELETE CASCADE
+);
+
+CREATE INDEX "IX_component_assembly_PartComponentId" ON metabase.component_assembly ("PartComponentId");
+
+CREATE INDEX "IX_component_variant_ToComponentId" ON metabase.component_variant ("ToComponentId");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20211110161448_AddReflexiveAssociationsToComponent', '5.0.11');
+
+COMMIT;
+
