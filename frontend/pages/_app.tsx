7eae196dc69a974055d17bad8c0e3c6aed202ff2
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-01-25 14:26:36 +0100

    Remove unused styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,9 +3,6 @@ import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apollo";
 import { CookiesProvider } from "react-cookie";
 
-import "../styles/vars.css";
-import "../styles/global.css";
-
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
