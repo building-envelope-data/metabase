2ca49f9af706c21614fd24abd0cd3f52f04c0af4
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-01-26 16:46:21 +0100

    Format code with `dotnet format`

--- backend/src/Startup.cs
+++ backend/src/Startup.cs
@@ -220,45 +220,45 @@ namespace Metabase
 
         // Inspired by https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-7.0#customize-output
         private static Task WriteJsonResponse(HttpContext context, HealthReport healthReport)
-{
-    context.Response.ContentType = "application/json; charset=utf-8";
-
-    var options = new JsonWriterOptions { Indented = true };
-
-    using var memoryStream = new MemoryStream();
-    using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
-    {
-        jsonWriter.WriteStartObject();
-        jsonWriter.WriteString("status", healthReport.Status.ToString());
-        jsonWriter.WriteStartObject("results");
-
-        foreach (var healthReportEntry in healthReport.Entries)
         {
-            jsonWriter.WriteStartObject(healthReportEntry.Key);
-            jsonWriter.WriteString("status",
-                healthReportEntry.Value.Status.ToString());
-            jsonWriter.WriteString("description",
-                healthReportEntry.Value.Description);
-            jsonWriter.WriteStartObject("data");
+            context.Response.ContentType = "application/json; charset=utf-8";
 
-            foreach (var item in healthReportEntry.Value.Data)
+            var options = new JsonWriterOptions { Indented = true };
+
+            using var memoryStream = new MemoryStream();
+            using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
             {
-                jsonWriter.WritePropertyName(item.Key);
+                jsonWriter.WriteStartObject();
+                jsonWriter.WriteString("status", healthReport.Status.ToString());
+                jsonWriter.WriteStartObject("results");
 
-                JsonSerializer.Serialize(jsonWriter, item.Value,
-                    item.Value?.GetType() ?? typeof(object));
+                foreach (var healthReportEntry in healthReport.Entries)
+                {
+                    jsonWriter.WriteStartObject(healthReportEntry.Key);
+                    jsonWriter.WriteString("status",
+                        healthReportEntry.Value.Status.ToString());
+                    jsonWriter.WriteString("description",
+                        healthReportEntry.Value.Description);
+                    jsonWriter.WriteStartObject("data");
+
+                    foreach (var item in healthReportEntry.Value.Data)
+                    {
+                        jsonWriter.WritePropertyName(item.Key);
+
+                        JsonSerializer.Serialize(jsonWriter, item.Value,
+                            item.Value?.GetType() ?? typeof(object));
+                    }
+
+                    jsonWriter.WriteEndObject();
+                    jsonWriter.WriteEndObject();
+                }
+
+                jsonWriter.WriteEndObject();
+                jsonWriter.WriteEndObject();
             }
 
-            jsonWriter.WriteEndObject();
-            jsonWriter.WriteEndObject();
+            return context.Response.WriteAsync(
+                Encoding.UTF8.GetString(memoryStream.ToArray()));
         }
-
-        jsonWriter.WriteEndObject();
-        jsonWriter.WriteEndObject();
-    }
-
-    return context.Response.WriteAsync(
-        Encoding.UTF8.GetString(memoryStream.ToArray()));
-}
     }
 }
