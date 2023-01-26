a3423d1246ec3c1416e0229dfd125cc791f287b8
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-01-26 16:45:26 +0100

    Create migration regarding upgrade of Npgsql to version 7

--- backend/src/Migrations/20230126154403_UpgradeNpqsqlToVersion7.Designer.cs
+++ backend/src/Migrations/20230126154403_UpgradeNpqsqlToVersion7.Designer.cs
@@ -4,6 +4,7 @@ using Metabase.Data;
 using Metabase.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using NpgsqlTypes;
 namespace Metabase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230126154403_UpgradeNpqsqlToVersion7")]
+    partial class UpgradeNpqsqlToVersion7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
