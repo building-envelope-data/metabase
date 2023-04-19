15a9abcea27a6f109dd0fdb70307c88ad0744857
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-04-19 13:40:58 +0200

    Set verification state to verified for existing databases

---
+++ backend/src/Migrations/20230419112023_InitializeDatabaseVerificationCodesAndSetVerificationStateToVerified.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDatabaseVerificationCodesAndSetVerificationStateToVerified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // [Manual migration customization](https://learn.microsoft.com/en-us/ef/core/modeling/data-seeding#manual-migration-customization)
+            // [Change data in migration Up method](https://stackoverflow.com/questions/33516705/change-data-in-migration-up-method-entity-framework/33516868#33516868)
+            migrationBuilder.Sql(@"UPDATE metabase.database SET ""VerificationState"" = 'verified';");
+            // migrationBuilder.UpdateData(
+            //     table: "database", 
+            //     keyColumn: "Id", 
+            //     keyValue: null,
+            //     column: "VerificationCode", 
+            //     value: Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE metabase.database SET ""VerificationState"" = 'pending';");
+        }
+    }
+}
