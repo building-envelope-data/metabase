71af6a8d1f987c34331c6eff864064c08be46f05
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-04-28 13:43:41 +0200

    Handle missing subject claim gracefully

--- backend/src/Controllers/UserinfoController.cs
+++ backend/src/Controllers/UserinfoController.cs
@@ -24,10 +24,19 @@ namespace Metabase.Controllers
         [HttpGet("~/connect/userinfo"), HttpPost("~/connect/userinfo"), Produces("application/json")]
         public async Task<IActionResult> Userinfo()
         {
-            var user = await _userManager.FindByIdAsync(
-                User.GetClaim(Claims.Subject)
-                ?? throw new InvalidOperationException("The subject claim of the claims principal is `null`.")
-                ).ConfigureAwait(false);
+            var subject = User.GetClaim(Claims.Subject);
+            if (subject is null)
+            {
+                return Challenge(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string?>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.InvalidRequest,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
+                            "The subject claim is missing."
+                    }));
+            }
+            var user = await _userManager.FindByIdAsync(subject).ConfigureAwait(false);
             if (user is null)
             {
                 return Challenge(
@@ -39,46 +48,33 @@ namespace Metabase.Controllers
                             "The specified access token is bound to an account that no longer exists."
                     }));
             }
-
             var claims = new Dictionary<string, object>(StringComparer.Ordinal)
             {
                 // Note: the "sub" claim is a mandatory claim and must be included in the JSON response.
                 [Claims.Subject] = await _userManager.GetUserIdAsync(user).ConfigureAwait(false)
             };
-
-            // var audience = User.GetClaim(Claims.Audience);
-            // if (audience is not null)
-            // {
-            //     claims[Claims.Audience] = audience;
-            // }
-
             if (User.HasScope(Scopes.Email))
             {
                 var email = await _userManager.GetEmailAsync(user).ConfigureAwait(false);
                 if (email is not null) claims[Claims.Email] = email;
                 claims[Claims.EmailVerified] = await _userManager.IsEmailConfirmedAsync(user).ConfigureAwait(false);
             }
-
-            if (User.HasScope(Scopes.Profile))
-            {
-                claims[Claims.Name] = user.Name;
-            }
-
             if (User.HasScope(Scopes.Phone))
             {
                 var phoneNumber = await _userManager.GetPhoneNumberAsync(user).ConfigureAwait(false);
                 if (phoneNumber is not null) claims[Claims.PhoneNumber] = phoneNumber;
                 claims[Claims.PhoneNumberVerified] = await _userManager.IsPhoneNumberConfirmedAsync(user).ConfigureAwait(false);
             }
-
+            if (User.HasScope(Scopes.Profile))
+            {
+                claims[Claims.Name] = user.Name;
+            }
             if (User.HasScope(Scopes.Roles))
             {
                 claims[Claims.Role] = await _userManager.GetRolesAsync(user).ConfigureAwait(false);
             }
-
             // Note: the complete list of standard claims supported by the OpenID Connect specification
             // can be found here: http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
-
             return Ok(claims);
         }
     }
