dc349cb317b197b179c1212016e86cc7a6a3bd55
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-05-30 16:42:49 +0200

    Link to an issue with gotchas regarding antiforgery tokens in ASP.NET Core

--- backend/src/Controllers/AntiforgeryController.cs
+++ backend/src/Controllers/AntiforgeryController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Metabase.Controllers
 {
+    // For gotchas regarding antiforgery tokens read
+    // [Clarity around IAntiforgery and ValidateAntiForgeryToken](https://github.com/dotnet/aspnetcore/issues/2783)
     public sealed class AntiforgeryController : Controller
     {
         private const string XsrfCookieKey = "XSRF-TOKEN";
