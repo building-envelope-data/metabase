304338f18a7e3cbb40db90b427e767a7d965a578
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2024-01-18 16:34:46 +0100

    Add database migrations for OpenIddict

---
+++ backend/src/Migrations/20240118143226_UpgradeOpenIddict.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeOpenIddict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                schema: "metabase",
+                table: "OpenIddictApplications",
+                newName: "ClientType");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationType",
+                schema: "metabase",
+                table: "OpenIddictApplications",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JsonWebKeySet",
+                schema: "metabase",
+                table: "OpenIddictApplications",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Settings",
+                schema: "metabase",
+                table: "OpenIddictApplications",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationType",
+                schema: "metabase",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.DropColumn(
+                name: "JsonWebKeySet",
+                schema: "metabase",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.DropColumn(
+                name: "Settings",
+                schema: "metabase",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientType",
+                schema: "metabase",
+                table: "OpenIddictApplications",
+                newName: "Type");
+        }
+    }
+}
