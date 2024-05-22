df11b8c92a623b13f8d95496d72a55a09b70fc53
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2024-05-22 13:22:34 +0200

    Upgrade dependencies

---
+++ backend/src/Migrations/20240514135851_ChangeUserNameColumn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "metabase",
+                table: "user",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "metabase",
+                table: "user",
+                type: "text",
+                nullable: false);
+        }
+    }
+}
