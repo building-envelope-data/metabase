f0998a32a4b3a17b04195b7b5a0e76eb5a683c51
Shreya Ponnamma <shreya.ponnamma@ise.fraunhofer.de>
2024-10-09 11:26:46 +0200

    migrations

---
+++ backend/src/Migrations/20240930122255_AddGeometricData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeometricData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
