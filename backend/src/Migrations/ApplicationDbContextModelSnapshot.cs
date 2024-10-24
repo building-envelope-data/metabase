a1b9b22ad39bbab20f26199f9203edf507a19529
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2024-10-24 18:36:01 +0200

    Remove empty migration regarding geometric data (there was no change in the PostgreSQL database because geometric data is fetched from product databases)

--- backend/src/Migrations/ApplicationDbContextModelSnapshot.cs
+++ backend/src/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -20,7 +20,7 @@ namespace Metabase.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("metabase")
-                .HasAnnotation("ProductVersion", "8.0.8")
+                .HasAnnotation("ProductVersion", "8.0.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.HasPostgresEnum(modelBuilder, "metabase", "component_category", new[] { "material", "layer", "unit" });
