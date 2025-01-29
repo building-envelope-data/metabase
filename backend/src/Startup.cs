37eb523eb921f01b6602cd9a53bc37afb9bb341e
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2025-01-29 18:50:59 +0100

    Improve OpenAPI document for health endpoint

--- backend/src/Startup.cs
+++ backend/src/Startup.cs
@@ -304,6 +304,8 @@ public sealed class Startup(
             }
         )
         .WithName("Health")
+        .WithDescription("Check the webserver health.")
+        .WithTags("Health")
         .DisableHttpMetrics();
     }
 
