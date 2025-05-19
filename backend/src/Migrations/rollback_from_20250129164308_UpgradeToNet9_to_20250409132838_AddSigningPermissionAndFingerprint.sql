a1298e7e0583b53f2ca3b06cb36a693cea04d753
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-05-19 18:25:05 +0200

    Add SQL migration and rollback scripts

---
+++ backend/src/Migrations/rollback_from_20250129164308_UpgradeToNet9_to_20250409132838_AddSigningPermissionAndFingerprint.sql
@@ -0,0 +1,12 @@
+﻿START TRANSACTION;
+ALTER TABLE metabase.institution_representative DROP COLUMN "DataSigningPermission";
+
+ALTER TABLE metabase.institution_representative DROP COLUMN "KeyFingerprints";
+
+DROP TYPE metabase.data_signing_permission;
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250409132838_AddSigningPermissionAndFingerprint';
+
+COMMIT;
+
