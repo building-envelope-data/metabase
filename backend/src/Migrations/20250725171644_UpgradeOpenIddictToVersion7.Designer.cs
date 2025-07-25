c0c53a995df92a187c5f0861d5d65f65fe4aa159
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-07-25 19:20:39 +0200

    Add migration to upgrade database for OpenIddict 7

--- backend/src/Migrations/20250725171644_UpgradeOpenIddictToVersion7.Designer.cs
+++ backend/src/Migrations/20250725171644_UpgradeOpenIddictToVersion7.Designer.cs
@@ -5,6 +5,7 @@ using Metabase.Data;
 using Metabase.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -14,9 +15,11 @@ using NpgsqlTypes;
 namespace Metabase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250725171644_UpgradeOpenIddictToVersion7")]
+    partial class UpgradeOpenIddictToVersion7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Migrations/rollback_from_20250714160511_InstitutionOpenIdConnectApplicationAssociationProperly_to_20250725171644_UpgradeOpenIddictToVersion7.sql
@@ -0,0 +1,8 @@
+﻿START TRANSACTION;
+ALTER TABLE metabase."OpenIddictTokens" ALTER COLUMN "Type" TYPE character varying(50);
+
+DELETE FROM "__EFMigrationsHistory"
+WHERE "MigrationId" = '20250725171644_UpgradeOpenIddictToVersion7';
+
+COMMIT;
+
