b3426f5fccc7ba7adb4ddc8f91414eb4aa8dc4b6
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-07-26 12:16:45 +0200

    Remove unused import

--- backend/src/GraphQl/Components/ComponentConcretizationOfConnection.cs
+++ backend/src/GraphQl/Components/ComponentConcretizationOfConnection.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Metabase.Authorization;
 using Metabase.Data;
 using Metabase.GraphQl.Users;
-using Microsoft.AspNetCore.Identity;
 
 namespace Metabase.GraphQl.Components;
 
