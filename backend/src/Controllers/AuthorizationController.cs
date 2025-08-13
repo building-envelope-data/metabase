0982d94cde4a543995cd6fa942200f0fd8706013
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-08-13 10:22:37 +0200

    Remove unnecessary extension `IAsyncEnumerable#ToListAsync` as it is part of the standard library

--- backend/src/Controllers/AuthorizationController.cs
+++ backend/src/Controllers/AuthorizationController.cs
@@ -719,28 +719,6 @@ public sealed class AuthorizationController(
     }
 }
 
-public static class AsyncEnumerableExtensions
-{
-    public static Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> source)
-    {
-        ArgumentNullException.ThrowIfNull(source);
-
-        return ExecuteAsync();
-
-        async Task<List<T>> ExecuteAsync()
-        {
-            var list = new List<T>();
-
-            await foreach (var element in source)
-            {
-                list.Add(element);
-            }
-
-            return list;
-        }
-    }
-}
-
 public sealed class FormValueRequiredAttribute(string name) : ActionMethodSelectorAttribute
 {
     private readonly string _name = name;
