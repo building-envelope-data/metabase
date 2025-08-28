4f7bb804e8a8feb7b77acbfd0906997f3b6c720a
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-08-28 12:36:05 +0200

    Create migration that aligns field names of fingerprint entities

--- backend/src/Migrations/20250828103451_AlignFieldNamesOfFingerprint.Designer.cs
+++ backend/src/Migrations/20250828103451_AlignFieldNamesOfFingerprint.Designer.cs
@@ -5,6 +5,7 @@ using Metabase.Data;
 using Metabase.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -14,9 +15,11 @@ using NpgsqlTypes;
 namespace Metabase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250828103451_AlignFieldNamesOfFingerprint")]
+    partial class AlignFieldNamesOfFingerprint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
