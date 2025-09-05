465e9ec90a69d1e4065c5da60c5071c07dcb8d13
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-09-05 18:32:39 +0200

    Add information for polymorphoic resolution when serializing and deserializing JSON from and into the domain model

--- backend/src/Data/IMethodDeveloper.cs
+++ backend/src/Data/IMethodDeveloper.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Text.Json.Serialization;
+using Metabase.Configuration;
 
 namespace Metabase.Data;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = GraphQlConfiguration.TypeDiscriminatorPropertyName)]
+[JsonDerivedType(typeof(UserMethodDeveloper), typeDiscriminator: nameof(UserMethodDeveloper))]
+[JsonDerivedType(typeof(InstitutionMethodDeveloper), typeDiscriminator: nameof(InstitutionMethodDeveloper))]
 public interface IMethodDeveloper
 {
     public Guid MethodId { get; }
--- backend/src/Data/IStakeholder.cs
+++ backend/src/Data/IStakeholder.cs
@@ -1,5 +1,11 @@
+using System.Text.Json.Serialization;
+using Metabase.Configuration;
+
 namespace Metabase.Data;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = GraphQlConfiguration.TypeDiscriminatorPropertyName)]
+[JsonDerivedType(typeof(User), typeDiscriminator: nameof(User))]
+[JsonDerivedType(typeof(Institution), typeDiscriminator: nameof(Institution))]
 public interface IStakeholder
 {
 }
