ca7f31299b7d09f0c5d647166477608ec47d4c3d
Simon Wacker <simon.wacker@ise.faunhofer.de>
2025-10-10 15:39:22 +0200

    Add missing import to allow fingerprint from signing

--- frontend/components/gnuPgKeyFingerprints/GnuPgKeyFingerprintTable.tsx
+++ frontend/components/gnuPgKeyFingerprints/GnuPgKeyFingerprintTable.tsx
@@ -2,6 +2,7 @@ import { Skeleton, Space, Table, TableProps } from "antd";
 import { GnuPgKeyFingerprintPartialFragment } from "../../queries/institutions.graphql";
 import Link from "next/link";
 import paths from "../../paths";
+import AllowGnuPgKeyFingerprint from "./AllowGnuPgKeyFingerprint"
 import ForbidGnuPgKeyFingerprint from "./ForbidGnuPgKeyFingerprint";
 import { Scalars } from "../../__generated__/__types__";
 
