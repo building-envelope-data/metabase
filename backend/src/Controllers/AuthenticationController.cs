f45b6530f1cc3d8efbb5937d004f8ef98a5df442
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-01-08 14:11:56 +0100

    Do not bind the cookie lifetime to the authentication-ticket lifetime

--- backend/src/Controllers/AuthenticationController.cs
+++ backend/src/Controllers/AuthenticationController.cs
@@ -17,7 +17,6 @@ using static OpenIddict.Abstractions.OpenIddictConstants;
 namespace Metabase.Controllers;
 
 // Inspired by https://github.com/openiddict/openiddict-samples/blob/dev/samples/Velusia/Velusia.Client/Controllers/AuthenticationController.cs
-// https://github.com/openiddict/openiddict-samples/blob/855c31f91d6bf5cde735ef3f96fcc3c015b51d79/samples/Velusia/Velusia.Client/Controllers/AuthenticationController.cs
 public sealed class AuthenticationController(
     AppSettings appSettings,
     IOptions<IdentityOptions> identityOptions
@@ -76,8 +75,7 @@ public sealed class AuthenticationController(
                     // While not required, the specification encourages sending an id_token_hint
                     // parameter containing an identity token returned by the server for this user.
                     [OpenIddictClientAspNetCoreConstants.Properties.IdentityTokenHint] =
-                        result.Properties.GetTokenValue(OpenIddictClientAspNetCoreConstants.Tokens
-                            .BackchannelIdentityToken)
+                        result.Properties.GetTokenValue(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken)
                 }
             )
             {
@@ -126,54 +124,87 @@ public sealed class AuthenticationController(
         // Such identities cannot be used as-is to build an authentication cookie in ASP.NET Core (as the
         // antiforgery stack requires at least a name claim to bind CSRF cookies to the user's identity) but
         // the access/refresh tokens can be retrieved using result.Properties.GetTokens() to make API calls.
-        if (result.Principal is not ClaimsPrincipal { Identity.IsAuthenticated: true })
+        if (result is not { Succeeded: true, Principal.Identity.IsAuthenticated: true })
         {
-            throw new InvalidOperationException(
-                "The external authorization data cannot be used for authentication.");
+            throw new InvalidOperationException("The external authorization data cannot be used for authentication.");
         }
 
         // Build an identity based on the external claims and that will be used to create the authentication cookie.
         var identity = new ClaimsIdentity(
-            CookieAuthenticationDefaults.AuthenticationScheme,
-            ClaimTypes.Name,
-            ClaimTypes.Role
+            authenticationType: "ExternalLogin",
+            nameType: ClaimTypes.Name,
+            roleType: ClaimTypes.Role
         );
 
         // By default, OpenIddict will automatically try to map the email/name and name identifier claims from
         // their standard OpenID Connect or provider-specific equivalent, if available. If needed, additional
-        // claims can be resolved from the external identity and copied to the final authentication cookie. For example
+        // claims can be resolved from the external identity and copied to the final authentication cookie.
+        // For example
         // .SetClaim(ClaimTypes.Email, result.Principal.GetClaim(ClaimTypes.Email))
         // The claims are fetched from the userinfo endpoint of the authorization provider.
         // We add the claim `IdentityOptions.ClaimsIdentity.UserIdClaimType` to make
         // `UserManager.GetUserAsync(claimsPrincipal)` return the authenticated user.
-        identity.SetClaim(_identityOptions.ClaimsIdentity.UserIdClaimType, result.Principal.GetClaim(ClaimTypes.NameIdentifier))
-                .SetClaim(ClaimTypes.Name, result.Principal.GetClaim(ClaimTypes.Name))
+        identity.SetClaim(ClaimTypes.Name, result.Principal.GetClaim(ClaimTypes.Name))
                 .SetClaim(ClaimTypes.NameIdentifier, result.Principal.GetClaim(ClaimTypes.NameIdentifier));
 
         // Preserve the registration details to be able to resolve them later.
         identity.SetClaim(Claims.Private.RegistrationId, result.Principal.GetClaim(Claims.Private.RegistrationId))
                 .SetClaim(Claims.Private.ProviderName, result.Principal.GetClaim(Claims.Private.ProviderName));
 
-        AuthenticationProperties properties;
-        if (result.Properties is null)
+        // Important: when using ASP.NET Core Identity and its default UI, the identity created in this action is
+        // not directly persisted in the final authentication cookie (called "application cookie" by Identity) but
+        // in an intermediate authentication cookie called "external cookie" (the final authentication cookie is
+        // later created by Identity's ExternalLogin Razor Page by calling SignInManager.ExternalLoginSignInAsync()).
+        //
+        // Unfortunately, this process doesn't preserve the claims added here, which prevents flowing claims
+        // returned by the external provider down to the final authentication cookie. For scenarios that
+        // require that, the claims can be stored in Identity's database by calling UserManager.AddClaimAsync()
+        // directly in this action or by scaffolding the ExternalLogin.cshtml page that is part of the default UI:
+        // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/additional-claims#add-and-update-user-claims.
+        //
+        // Alternatively, if flowing the claims from the "external cookie" to the "application cookie" is preferred,
+        // the default ExternalLogin.cshtml page provided by Identity can be scaffolded to replace the call to
+        // SignInManager.ExternalLoginSignInAsync() by a manual sign-in operation that will preserve the claims.
+        // For scenarios where scaffolding the ExternalLogin.cshtml page is not convenient, a custom SignInManager
+        // with an overridden SignInOrTwoFactorAsync() method can also be used to tweak the default Identity logic.
+        //
+        // For more information, see https://haacked.com/archive/2019/07/16/external-claims/ and
+        // https://stackoverflow.com/questions/42660568/asp-net-core-identity-extract-and-save-external-login-tokens-and-add-claims-to-l/42670559#42670559.
+
+        // Build the authentication properties based on the properties that were added when the challenge was triggered.
+        var properties = new AuthenticationProperties(result.Properties.Items)
         {
-            properties = new AuthenticationProperties();
-        }
-        else
-        {
-            // Build the authentication properties based on the properties that were added when the challenge was triggered.
-            properties = new AuthenticationProperties(result.Properties.Items);
-            // If needed, the tokens returned by the authorization server can be stored in the authentication cookie.
-            // To make cookies less heavy, tokens that are not used are filtered out before creating the cookie.
-            properties.StoreTokens(result.Properties.GetTokens().Where(token => token.Name is
+            RedirectUri = result.Properties.RedirectUri ?? "/",
+            // Set the creation and expiration dates of the ticket to null to decorrelate the lifetime
+            // of the resulting authentication cookie from the lifetime of the identity token returned by
+            // the authorization server (if applicable). In this case, the expiration date time will be
+            // automatically computed by the cookie handler using the lifetime configured in the options.
+            //
+            // Applications that prefer binding the lifetime of the ticket stored in the authentication cookie
+            // to the identity token returned by the identity provider can remove or comment these two lines:
+            IssuedUtc = null,
+            ExpiresUtc = null,
+            // Note: this flag controls whether the authentication cookie that will be returned to the
+            // browser will be treated as a session cookie (i.e destroyed when the browser is closed)
+            // or as a persistent cookie. In both cases, the lifetime of the authentication ticket is
+            // always stored as protected data, preventing malicious users from trying to use an
+            // authentication cookie beyond the lifetime of the authentication ticket itself.
+            IsPersistent = false
+        };
+
+        // If needed, the tokens returned by the authorization server can be stored in the authentication cookie.
+        //
+        // To make cookies less heavy, tokens that are not used are filtered out before creating the cookie.
+        properties.StoreTokens(
+            result.Properties.GetTokens().Where(token => token.Name is
                 // Preserve the access, identity and refresh tokens returned in the token response, if available.
                 OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken or
                 OpenIddictClientAspNetCoreConstants.Tokens.BackchannelIdentityToken or
                 OpenIddictClientAspNetCoreConstants.Tokens.RefreshToken
-            ));
-        }
+            )
+        );
 
-        // Ask the cookie authentication handler to return a new cookie and redirect
+        // Ask the cookie authentication sign-in handler to return a new cookie and redirect
         // the user agent to the return URL stored in the authentication properties.
         return SignIn(
             new ClaimsPrincipal(identity),
