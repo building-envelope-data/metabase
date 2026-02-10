bd69fe858d3b64b2b755d238c5949edff39ed467
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-02-10 15:22:03 +0100

    Ignore stored target and pass variables explicitly

--- deploy.mk
+++ deploy.mk
@@ -18,6 +18,14 @@ dotenv_linter = \
 		--quiet \
 		dotenvlinter/dotenv-linter:latest
 
+# Taken from https://www.client9.com/self-documenting-makefiles/
+help : ## Print this help
+	@awk -F ':|##' '/^[^\t].+?:.*?##/ {\
+		printf "\033[36m%-30s\033[0m %s\n", $$1, $$NF \
+	}' $(MAKEFILE_LIST)
+.PHONY : help
+.DEFAULT_GOAL := help
+
 # To deploy `develop`, I usually just take all docker services down by running
 # `cd /app/machine; make down; cd /app/production; make down; cd /app/staging; make down;`
 # pull the latest code by running
@@ -33,10 +41,10 @@ dotenv_linter = \
 deploy : DIR = "$(shell pwd)/backup"
 deploy : symlink ## Deploy tag, branch, or commit `${TARGET}`, for example, `./deploy.mk deploy TARGET=v1.0.0`
 	$(MAKE) --file="${SELF}" begin-maintenance
-	$(MAKE) --file="${SELF}" store-target set-target
-	$(MAKE) --file="${SELF}" backup
+	$(MAKE) --file="${SELF}" store-target set-target TARGET="${TARGET}"
+	$(MAKE) --file="${SELF}" backup DIR="${DIR}"
 	$(MAKE) --file="${SELF}" fetch-all
-	$(MAKE) --file="${SELF}" checkout
+	$(MAKE) --file="${SELF}" checkout TARGET="${TARGET}"
 	$(MAKE) --file="${SELF}" dotenv
 	$(MAKE) --file="${SELF}" migrate
 	$(MAKE) --file="${SELF}" services
@@ -48,10 +56,10 @@ rollback : TARGET = $(shell cat ./.stored-target)
 rollback : DIR = "$(shell pwd)/backup"
 rollback : symlink ## Rollback deployment attempt (uses target stored in `./.stored-target` and database backup stored in `./backup/`)
 	$(MAKE) --file="${SELF}" begin-maintenance
-	$(MAKE) --file="${SELF}" set-target
-	$(MAKE) --file="${SELF}" checkout
+	$(MAKE) --file="${SELF}" set-target TARGET="${TARGET}"
+	$(MAKE) --file="${SELF}" checkout TARGET="${TARGET}"
 	$(MAKE) --file="${SELF}" dotenv
-	$(MAKE) --file="${SELF}" restore
+	$(MAKE) --file="${SELF}" restore DIR="${DIR}"
 	$(MAKE) --file="${SELF}" services
 	$(MAKE) --file="${SELF}" run-tests
 	$(MAKE) --file="${SELF}" end-maintenance
@@ -79,23 +87,22 @@ set-target : ## Set variable `TARGET` in ./.env to `${TARGET}`
 .PHONY : set-target
 
 backup : ## Backup database and related data to the directory with the absolute path `${DIR}` (down-ing and up-ing the database service before and after to prevent race conditions), for example, `./deploy.mk backup DIR=/app/data/backups/$(date +"%Y-%m-%d_%H_%M_%S")`
-	$(MAKE) --file="${SELF}" \
-		--file="$(shell pwd)/database.mk" \
-		DIR=${DIR} \
+	$(MAKE) --file=./database.mk \
+		DIR="${DIR}" \
 		backup
 .PHONY : backup
 
 restore : CONTAINER_NAME = restore_${NAME}_database
 restore : ## Restore database and related data from the directory with the absolute path `${DIR}` (down-ing and up-ing the database service before and after to prevent race conditions and removing and recreating the data volume before to start cleanly), for example, `./deploy.mk restore DIR=/app/data/backups/2021-04-22_15_43_35/` (note that after restoring a database it is usually necessary to restart the backend service for the object-relational mapper Npgsql to work seamlessly, for example, by restarting all services with `./docker.mk restart`)`
 	$(MAKE) --file="${SELF}" \
-		--file="$(shell pwd)/database.mk" \
+		--file=./database.mk \
 		DIR=${DIR} \
 		backup
 .PHONY : restore
 
 migrate : ## Migrate database (down-ing and up-ing the database service before and after to prevent race conditions. In general, note that other PostgreSQL instances using the same data volume must not be used while migrating and need to be restarted afterwards to make migration results visible)
 	$(MAKE) --file="${SELF}" \
-		--file="$(shell pwd)/database.mk" \
+		--file=./database.mk \
 		migrate
 .PHONY : migrate
 
