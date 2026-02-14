93545b5036ed536e187f1886feb21f08856c6439
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-02-14 09:19:41 +0100

    Add missing database name to pg_dump

--- database.mk
+++ database.mk
@@ -128,6 +128,7 @@ backup : ## Backup database and related data to directory with absolute path `${
 			--clean \
 			--if-exists \
 			--username="${POSTGRES_USER}" \
+			--dbname="${POSTGRES_DATABASE_NAME}" \
 		| gzip \
 		> "${DIR}/${dump_archive_name}"
 	docker container stop "${CONTAINER_NAME}"
