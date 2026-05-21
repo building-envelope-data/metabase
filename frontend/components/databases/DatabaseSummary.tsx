7d010fd4ad12988a9bc0c848e21afab4f4313b71
christoph-maurer <christoph.maurer@ise.fraunhofer.de>
2026-05-21 17:03:00 +0200

    Exchange `route={paths.database` by `institution`

--- frontend/components/databases/DatabaseSummary.tsx
+++ frontend/components/databases/DatabaseSummary.tsx
@@ -50,7 +50,7 @@ export default function DatabaseSummary({
       ].filter(isTruthy)}
     >
       <div>
-        Operated by <EntityLink entity={entity} route={paths.database} />
+        Operated by <EntityLink entity={entity} route={paths.institution} />
       </div>
       <div>
         <Typography.Link href={entity.locator}>
