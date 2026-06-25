73f4146dae1e1b92fa9e77be537d453dee1a7859
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-06-25 15:07:05 +0200

    Drop the as-of-now not-needed column `Contact_Exists` from `institution` if it should still exist for some reason

--- backend/src/Migrations/migrate.sql
+++ backend/src/Migrations/migrate.sql
@@ -2865,3 +2865,22 @@ BEGIN
 END $EF$;
 COMMIT;
 
+START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260625115427_DropContactExistsColumnFromInstitution') THEN
+    ALTER TABLE metabase.institution DROP COLUMN IF EXISTS "Contact_Exists";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260625115427_DropContactExistsColumnFromInstitution') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260625115427_DropContactExistsColumnFromInstitution', '10.0.9');
+    END IF;
+END $EF$;
+
+COMMIT;
+
