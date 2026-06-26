58585beabab4f0a161f81af8de4f71b033847f30
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-06-26 14:39:36 +0200

    Adapt the help target such that it does not choke on the character `:` when used within a target description

--- database.mk
+++ database.mk
@@ -13,7 +13,7 @@ dump_archive_name = postgresql_dumpall.gz
 
 # Taken from https://www.client9.com/self-documenting-makefiles/
 help : ## Print this help
-	@awk -F ':|##' '/^[^\t].+?:.*?##/ {\
+	@awk -F ':.*?## ' '/^[^\t].+?:.*?##/ {\
 		printf "\033[36m%-30s\033[0m %s\n", $$1, $$NF \
 	}' $(MAKEFILE_LIST)
 .PHONY : help
--- docker.mk
+++ docker.mk
@@ -20,7 +20,7 @@ dotenv_linter = \
 
 # Taken from https://www.client9.com/self-documenting-makefiles/
 help : ## Print this help
-	@awk -F ':|##' '/^[^\t].+?:.*?##/ {\
+	@awk -F ':.*?## ' '/^[^\t].+?:.*?##/ {\
 		printf "\033[36m%-30s\033[0m %s\n", $$1, $$NF \
 	}' $(MAKEFILE_LIST)
 .PHONY : help
